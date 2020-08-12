0f8f8a4e292c42fe3d58ee6046eea792f18ec097
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-08-13 03:40:54 +0530

    Add background img to scene

--- src/index.js
+++ src/index.js
@@ -2,10 +2,26 @@ import './styles/style.css';
 
 let gameScene = new Phaser.Scene('Game');
 
+gameScene.preload = function () {
+  this.load.image('background', 'assets/background.png');
+  this.load.image('player', 'assets/player.png');
+};
+
+gameScene.create = function () {
+  let bg = this.add.sprite(0, 0, 'background');
+  let gameW = this.sys.game.config.width;
+  let gameH = this.sys.game.config.height;
+
+
+  bg.setPosition(gameW / 2, gameH / 2);
+
+
+};
+
 let config = {
   type: Phaser.AUTO, 
-  width: 640,
   height: 360,
+  width: 640,
   scene: gameScene
 };
 
