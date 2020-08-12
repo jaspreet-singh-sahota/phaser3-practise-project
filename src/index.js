b9324c027bec1998d4cfcaf279e55557af8b5d18
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-08-13 04:06:30 +0530

    Add player

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ let gameScene = new Phaser.Scene('Game');
 gameScene.preload = function () {
   this.load.image('background', 'assets/background.png');
   this.load.image('player', 'assets/player.png');
+  this.load.image('enemy', 'assets/dragon.png');
 };
 
 gameScene.create = function () {
@@ -15,6 +16,10 @@ gameScene.create = function () {
 
   bg.setPosition(gameW / 2, gameH / 2);
 
+  let player = this.add.sprite(gameW/4 , gameH/2, 'player')
+
+  player.setScale(0.5, 0.5)
+  player.x = 50
 
 };
 
